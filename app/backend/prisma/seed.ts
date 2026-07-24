f2c4e53b233096fa682c491394f3044c35300538
stone alakou <48372272+alakou@users.noreply.github.com>
2026-07-24 02:17:41 +0200

    prisma config ok -  postgresql ok - db first crud ok

--- app/backend/prisma/seed.ts
+++ app/backend/prisma/seed.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 import { PrismaClient } from '../generated/prisma/client.js';
-
+console.log(process.env.DATABASE_URL);
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
